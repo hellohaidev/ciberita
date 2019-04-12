304b9c5dcf64c7975303cc16351389062cdf8efe
adirahman <kodingku@github.com>
2019-04-12 09:41:57 +0700

    create dan upload controller login

---
+++ application/controllers/Login.php
@@ -0,0 +1,71 @@
+<?php 
+
+
+class Login extends CI_Controller {
+
+
+    public function index() {
+    
+        $this->load->view('login');
+    
+    }
+
+    public function access() {
+
+        $this->form_validation->set_rules('username','Username','trim|required');
+        $this->form_validation->set_rules('password','Password','trim|required');
+
+        if ($this->form_validation->run()==FALSE)
+        {
+            $data = array('errors' => validation_errors());
+            $this->session->set_flashdata($data);
+            redirect('Login');
+        }
+        else {
+
+            $username = $this->input->post('username');
+            $password = $this->input->post('passsword');
+            $datauser = $this->users_model->login_user($username,$password);
+
+            if ($datauser) {
+                $userdata = array(
+                    'user_id' => $datauser,
+                    'username' => $username,
+                    'password' => $password,
+                    'logged_in' => true
+                );
+
+                $this->session->set_userdata($userdata);
+                $this->session->set_flashdata('login_success','anda berhasil login');
+                redirect('Dashboard');
+            }
+            else {
+                $this->session->set_flashdata('login_failed','maaf username atau password anda salah');
+                redirect('Login');
+            }
+
+        }
+
+    }
+
+    public function logout() {
+
+        $this->session->sess_destroy();
+        redirect('Login');
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+?>
