03189d6d8ff5a2f7274dc6beb6a69c7476821f9b
adirahman <kodingku@github.com>
2019-04-21 18:32:44 +0700

    pembuatan users model admin

--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -2,7 +2,23 @@
 
     class Users_model extends CI_Model {
 
-        
+        public function login_user($username,$password)
+		{
+
+			$this->db->where('username',$username);
+			$result = $this->db->get('users_ci');
+			$db_password = $result->row(3)->password;
+			if (password_verify($password,$db_password)) {
+					return $result->row(0)->id_users;
+				}
+			else {
+
+					return false;
+
+				}
+
+
+		}
 
 
     }
